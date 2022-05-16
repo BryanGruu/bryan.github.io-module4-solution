b658533739eb959f554a8a7aba37576392b346c8
Bryan Giancarlo Pelén Ramire 5to Baco <gian.pelen18@gmail.com>
2022-05-16 14:47:38 -0600

    tareagit

---
+++ scrip.js
@@ -0,0 +1,17 @@
+var names=new Array();
+names[]="yaakov"
+names[]="John"
+names[]="Jen"
+names[]="Jason"
+names[]="Paul"
+names[]="Frank"
+names[]="Larry"
+names[]="Paula"
+names[]="Jim"
+names[]="Gian"
+
+
+for (var i= 0; i < names.length; i++) {
+	if(names[i].charAt(0)===´'j'  names [i].charAt(0)==='j'){
+		console.log("Goodbye"+names[i])
+		}
